bc74a7cd21c0de465cc495ab61157b45c64e37fd
Lewechi Godsfavour <lewechigodsfavour@gmail.com>
2024-06-04 21:52:41 +0100

    Initial Commit

---
+++ Swift Component/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Component
+//
+//  Created by Lewechi Godsfavour on 04/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Component/Swift_ComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ComponentApp.swift
+//  Swift Component
+//
+//  Created by Lewechi Godsfavour on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
